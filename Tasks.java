532513d131b9bd4c044add02456f74e4aa9c0271
AdamG <adamgan0527@gmail.com>
2015-09-29 21:31:43 +0800

    remove duplicate constructor

--- Tasks.java
+++ Tasks.java
@@ -39,11 +39,6 @@ public class Tasks {
 		setTaskID(taskID);
 	}
 	
-	//for searching
-	public Tasks(String desc) {
-		setDescription(desc);
-	}
-	
 	//select a blocked timeslot as final, position starts from 1 (arraylist[0])
 	//returns true if selection was successful, false = task was not blocked task or invalid selection index
 	public boolean selectBlockedSlot(int position) {
