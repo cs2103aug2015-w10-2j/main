e4103fc1bf4ad38a2880348fcfef0a607c6826af
DINH UYEN <dhpuyen@users.noreply.github.com>
2015-10-01 02:49:49 +0800

    Update Time4WorkCLI.java

--- Time4WorkCLI.java
+++ Time4WorkCLI.java
@@ -1,57 +1,28 @@
 import java.util.*;
-import UserInterface;
-import Logic;
-import FeedbackMessage;
-import Tasks;
-import DeadLines;
 
-public class Time4WorkCLI implements UserInterface {
+public class Time4WorkCLI {
 	private static final String WELCOME_MESSAGE = "Welcome to Time4Work!";
 	private static final Scanner scanner = new Scanner(System.in);
-	private Logic cmdHandler = new Logic();
-
-	@Override
-	public void run() {
+	private static Logic cmdHandler = new Logic();
+	
+	public static void main (String[] args) throws IOException {
 		System.out.println(WELCOME_MESSAGE);
+		run();
+	}
+	
+	public static void run() throws IOException {
 		while (true) {
 			String userCommand = scanner.nextLine();
-			FeedbackMessage feedback = cmdHandler.executeCommand(userCommand);
-			showToUser(feedback);
+			FeedbackMessage feedback;
+            		feedback = cmdHandler.executeCommand(userCommand);
+            		showToUser(feedback);
 		}
 	}
 
 	/**
 	 * this method to return the result after user command was run.
 	 */
-	private void showToUser(FeedbackMessage feedback) {
+	private static void showToUser(FeedbackMessage feedback) {
 		System.out.println(feedback.getFeedback());
-		System.out.println(getContent(feedback.getTask()));
-	}
-
-	private static String displayTask(Tasks task) {
-		String description = task.getDescription();
-		/*
-		int type = task.getType(); //1 = deadline task, 2 = duration, 3 = blocked, 4 = floating
-		DeadLines deadlines = task.getDueDetails(); 
-		String startTime = deadlines.getStartTime(); 
-		String endTime = deadlines.getEndTime();
-		String startDate = deadlines.getStartDate();
-		String endDate = deadlines.getEndDate();
-		int deadlinesType= deadlines.getType(); //1 = deadline task, 2 = duration
-		*/
-		String taskItem = description + "";
-		return taskItem;
-	}
-	
-	public static String getContent(ArrayList<Tasks> taskList) {
-		int index;
-		String taskToDisplay = new String();
-
-		for (int i = 0; i < taskList.size(); i++) {
-			String taskItem = displayTask(taskList.get(i));
-			index = i + 1;
-			taskToDisplay += (index + ". " + taskItem +"\n");
-		}
-		return taskToDisplay;
 	}
 }
