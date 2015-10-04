78eeb185ba1eaa99ad8024d87055bdf08d2acec1
ongzlalan <ongzl.alan@gmail.com>
2015-10-04 20:51:57 +0800

    Added specific commands for functions besides add and update

--- Command.java
+++ Command.java
@@ -2,10 +2,30 @@ public class Command {
   
   private Tasks task;
   private String command;
+  private String searchKeyword;
+  private int indexToBeDeleted;
   
+  //for adding and updating
   public Command(String command, Tasks task) {
     this.command = command;
     this.task = task;
+  }
+  
+  //for searching
+  public Command(String command, String searchKeyword){
+	  this.command = command;
+	  this.searchKeyword = searchKeyword;
+  }
+  
+  //for deleting
+  public Command(String command, int indexToBeDeleted){
+	  this.command = command;
+	  this.indexToBeDeleted = indexToBeDeleted;
+  }
+  
+  //for exit and invalid commands
+  public Command(String command){
+	  this.command = command;
   }
   
   public Tasks getTask(){
@@ -15,4 +35,13 @@ public class Command {
   public String getCommand(){
 	  return command;
   }
+  
+  public String getSearchKeyword() {
+	  return searchKeyword;
+  }
+
+  public int getIndexToBeDeleted() {
+	  return indexToBeDeleted;
+  }
+  
 }
