533e0fad5250ba71426eb017f40cecc58feac2d0
LI ZAN <lizanmaggie@gmail.com>
2015-10-06 20:43:55 +0800

    revise display method

--- UIDemo.java
+++ UIDemo.java
@@ -6,73 +6,73 @@ import java.util.ArrayList;
 import javax.swing.*;
 
 public class UIDemo extends JPanel implements ActionListener {
-
-	private JTextField commandInput;
+    
+    private JTextField commandInput;
     private static JTextArea taskContent;
     private JLabel feedback;
     private static Logic logic = new Logic();
     private static ArrayList<Tasks> currentList = new ArrayList<Tasks>();
-
+    
     public UIDemo() {
         super(new GridBagLayout());
-
+        
         commandInput = new JTextField(50);
         commandInput.addActionListener(this);
-
+        
         feedback = new JLabel();
-
+        
         taskContent = new JTextArea(25, 50);
         taskContent.setEditable(false);
         JScrollPane scrollPane = new JScrollPane(taskContent);
-
+        
         //Add Components to this panel.
         GridBagConstraints c = new GridBagConstraints();
         c.gridwidth = GridBagConstraints.REMAINDER;
-
+        
         c.fill = GridBagConstraints.HORIZONTAL;
         add(commandInput, c);
         add(feedback, c);
-
+        
         c.fill = GridBagConstraints.BOTH;
         c.weightx = 1.0;
         c.weighty = 1.0;
         add(scrollPane, c);
-
+        
         currentList = logic.getCurrentList();
-		taskContent.setText(display(currentList));
+        taskContent.setText(display(currentList));
     }
-
+    
     public void actionPerformed(ActionEvent evt) {
-		try {
-			String userCommand = commandInput.getText();
-
-			FeedbackMessage output;
-			output = logic.executeCommand(userCommand);
-			currentList = logic.getCurrentList();
-			taskContent.setText(display(currentList));
-			feedback.setText(output.getFeedback());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        try {
+            String userCommand = commandInput.getText();
+            
+            FeedbackMessage output;
+            output = logic.executeCommand(userCommand);
+            currentList = logic.getCurrentList();
+            taskContent.setText(display(currentList));
+            feedback.setText(output.getFeedback());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         commandInput.selectAll();
-
+        
         //Make sure the new text is visible, even if there
         //was a selection in the text area.
         taskContent.setCaretPosition(taskContent.getDocument().getLength());
     }
-
+    
     public static String display(ArrayList<Tasks> taskList){
         String taskListContent = "";
         int taskListSize = taskList.size();
         for (int i = 0; i < taskListSize - 1; i++){
             taskListContent += (i+1) + ". " + taskList.get(i).getDescription() + "\n";
         }
-        taskListContent += taskList.get(taskListSize - 1);
+        taskListContent += taskListSize + ". " + taskList.get(taskListSize - 1).getDescription();
         return taskListContent;
     }
-
-      /**
+    
+    /**
      * Create the GUI and show it.  For thread safety,
      * this method should be invoked from the
      * event dispatch thread.
@@ -81,14 +81,14 @@ public class UIDemo extends JPanel implements ActionListener {
         //Create and set up the window.
         JFrame frame = new JFrame("Time4WorkDemo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-       frame.add(new UIDemo());
-
+        
+        frame.add(new UIDemo());
+        
         //Display the window.
         frame.pack();
         frame.setVisible(true);
     }
-
+    
     public static void main(String[] args) {
         //Schedule a job for the event dispatch thread:
         //creating and showing this application's GUI.
