b63b8f50d19b928758c39182949f91b4550cc9b9
AdamG <adamgan0527@gmail.com>
2015-10-10 13:45:53 +0800

    updated storage to handle custom path, updated Storage with facade better exception handling added search by completion and search by task type

--- Storage.java
+++ Storage.java
@@ -6,458 +6,81 @@ import com.google.gson.JsonSyntaxException;
 
 public class Storage {
 	
-	private File myFile;
-	private String defPath = "myTasks.txt";
-	private String currentPath = "";
-	private FileWriter fw;
-	private FileReader fr; 
-	private BufferedWriter bw;
-	private BufferedReader br;
+	private StorageLogic myLogic = new StorageLogic();
 	
-	private int type = 0;
-	private final int DeadlineType = 1;
-	private final int DurationType = 2;
-	private final int BlockedType = 3;
-	private final int FloatingType = 4;
+	//use storage default path, <local directory>/myTasks.txt
+	public Storage() throws IOException {		
+		myLogic.createDefaultFile();		
+	}
 	
-	Gson gson = new Gson(); 
+	//custom path and filename, path has to have escape characters
+	//eg. C:\\user\\Desktop\\myTasks.txt
+	public Storage(String path) throws IOException {		
+		myLogic.createCustomFile(path);		
+	}
 	
-	//default path
-	public Storage() {
-		setCurrentPath(defPath);
+	//read from file, deserialize and returns all tasks
+	public ArrayList<Tasks> readFile() throws IOException {		
 		try {
-			createFile(currentPath);
+			return myLogic.getAllTasks();
 		} catch (IOException e) {
-			e.printStackTrace();
-		}
+			throw e;
+		}		
 	}
-	
-	//specified path without fileName
-	public Storage(String path) {
-		currentPath = path + File.separator + "myTasks.txt";
+
+	//adds new task at the end of the list, returns back the new task if successful
+	public Tasks appendTask(Tasks newTask) throws IOException {		
 		try {
-			createFile(currentPath);
+			return myLogic.addNewTask(newTask);
 		} catch (IOException e) {
-			e.printStackTrace();
+			throw e;
 		}
+	}	
+	
+	//deletes the input taskID, returns the deleted task, returns null if no matching taskID
+	public Tasks deleteTask(int taskID) throws IOException, InterruptedException{		
+		try {
+			return myLogic.delete(taskID);
+		} catch (IOException | InterruptedException e) {
+			throw e;
+		}		
 	}
 	
-	//if file doesn't exist, returns null
-	//if file exists, return contents
-	private ArrayList<Tasks> createFile(String path) throws IOException {
-		
-		ArrayList<Tasks> myTaskList = new ArrayList<Tasks>();
-		
-		//set up file
-		myFile = new File(currentPath);
-		
-		//file not found, create file, intermediate directory? and return null
-		if(!myFile.exists()) {
-			//myFile.mkdirs();
-			myFile.createNewFile();
-		}
-		//file exists, read file and return contents
-		else {
-			myTaskList = readFile();
-		}
-		return myTaskList;
-
+	//search and returns entries with task description matching search String, null if no matches
+	public ArrayList<Tasks> SearchTask(String searchString) throws IOException{		
+		try {
+			return myLogic.searchDescription(searchString);
+		} catch (IOException e) {
+			throw e;
+		}		
 	}
-
-	private void openWriterReader() throws IOException, FileNotFoundException {
-		//set up writers
-		fw = new FileWriter(myFile.getAbsoluteFile(),true);
-		fr = new FileReader(myFile.getAbsoluteFile());
-		bw = new BufferedWriter(fw);
-		br = new BufferedReader(fr);
-	}
-	
-	//read from file, deserialize and add to myTaskList
-	public ArrayList<Tasks> readFile() {
-		
-		try {
-			openWriterReader();
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		
-		ArrayList<Tasks> myTaskList = new ArrayList<Tasks>();
-		String tempLine = "";
-		Tasks tempTask;
-		
-		//read contents of file
-		//String segments[] = path.split("\"type\":");
-		String splitter = "\"type\":";
-		
-		
-		try {
-			while ((tempLine = br.readLine()) != null) {
-				//extract type
-				int startIndex = tempLine.indexOf(splitter)+splitter.length();
-				type = Integer.parseInt( tempLine.substring(startIndex, startIndex+1));
-				
-				switch(type) {
-					case DeadlineType: 	tempTask = gson.fromJson(tempLine, DeadlineTask.class);
-										break;
-					case DurationType: 	tempTask = gson.fromJson(tempLine, DurationTask.class);
-										break;
-					case FloatingType: 	tempTask = gson.fromJson(tempLine, FloatingTask.class);
-										break;
-					case BlockedType: 	tempTask = gson.fromJson(tempLine, BlockedTask.class);
-										break;
-					default:			tempTask = null;
-										break;
-				}
-				//tempTask = gson.fromJson(tempLine, Tasks.class);
-				myTaskList.add(tempTask);
-			}
-		}
-		catch (JsonSyntaxException e) {
-			e.printStackTrace();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		//close writer and reader
-		try {
-			closeWriterReader();
-		} 
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		   
-		return myTaskList;
-	}
-
-	public Tasks appendTask(Tasks newTask) {
-		
-		try {
-			openWriterReader();
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		
-		
-		//if task has no taskID, generate one!
-		if(newTask.getTaskID() == 0) {
-			newTask.setTaskID(GenerateTaskID());
-		}
-		
-		try {
-			closeWriterReader();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		String tempLine = gson.toJson(newTask);
-		
-		try {
-			openWriterReader();
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		
-		//write new Tasks into file
-		try {
-			bw.write(tempLine);
-			bw.newLine();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		//close writer
-		try {
-			closeWriterReader();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return newTask;
-	}
-
-	
-	
-	public Tasks deleteTask(int taskID) throws IOException{
-		
-		Tasks deletedTask = null;
-		ArrayList<Tasks> myTaskList = new ArrayList<Tasks>();
-		
-		try {
-			openWriterReader();
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		
-		boolean needDelete = false;
-		myTaskList = readFile();
-		
-		//find if taskID to be deleted is found
-		for(int i=0; i<myTaskList.size(); i++) {
-			if(myTaskList.get(i).getTaskID() == taskID) {
-				needDelete = true;
-				deletedTask = myTaskList.get(i);
-				myTaskList.remove(i);
-				break;
-			}
-		}
-		
-		//taskID to be deleted is found
-		if(needDelete) {
-			
-			try {
-				closeWriterReader();
-				System.gc();
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-			//wait for garbage collector
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			
-			//delete old file
-			if (!myFile.delete()) {
-		        System.out.println("Could not delete file");
-		    } 
-			
-			createFile(currentPath);
-			
-			try {
-				openWriterReader();
-			} catch (FileNotFoundException e1) {
-				e1.printStackTrace();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-			
-			
-			
-			for(int i=0; i<myTaskList.size(); i++) {
-				String tempLine = gson.toJson(myTaskList.get(i)); 
-			
-				//write new Tasks into file
-				try {
-					bw.write(tempLine);
-					bw.newLine();
-				} 
-				catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		
-		try {
-			closeWriterReader();
-			System.gc();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-				/*
-			//create temporary file without deleted entry
-			File tempFile = new File(myFile.getAbsolutePath() + ".tmp");
-			FileWriter TempFw = new FileWriter(tempFile.getAbsoluteFile(), true);
-			BufferedWriter TempBw = new BufferedWriter(TempFw);
-			
-			for(int i=0; i<myTaskList.size(); i++) {
-				String tempLine = gson.toJson(myTaskList.get(i)); 
-			
-				//write new Tasks into file
-				try {
-					TempBw.write(tempLine);
-					TempBw.newLine();
-				} 
-				catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			
-			//close all writers and readers
-			try {
-				TempBw.close();
-				closeWriterReader();
-				System.gc();
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-			//wait for garbage collector
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			
-			//delete original file
-			if (!myFile.delete()) {
-		        System.out.println("Could not delete file");
-		      } 
-		      
-		    //Rename the new file to the filename the original file had.
-		    if (!tempFile.renameTo(myFile)) {
-		    	System.out.println("Could not rename file");
-		    }
-		}	
-		*/
-		return deletedTask;
-	}
-	
-	//search and returns entries with description matching search String
-	public ArrayList<Tasks> SearchTask(String searchString){
-		
-		try {
-			openWriterReader();
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		
-		
-		ArrayList<Tasks> tempList = readFile();
-		ArrayList<Tasks> myTaskList = new ArrayList<Tasks>();
-		
-		for(int i=0; i<tempList.size(); i++) {
-			if(tempList.get(i).getDescription().contains(searchString)) {
-				myTaskList.add(tempList.get(i));
-			}
-		}
-		
-		try {
-			closeWriterReader();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return myTaskList;
-	}
-	
-	//search and returns task by taskID
-		public Tasks SearchTaskID(int taskID) throws FileNotFoundException, IOException{
-			
-			openWriterReader();
-
-			
-			ArrayList<Tasks> tempList = readFile();
-			Tasks myTask = null;
-			
-			for(int i=0; i<tempList.size(); i++) {
-				if(tempList.get(i).getTaskID() == taskID) {
-					myTask = tempList.get(i);
-				}
-			}
-			
-			closeWriterReader();
-			
-			return myTask;
-		}
 	
 	//replaces specified taskID with updated Tasks and returns "old" updated task
-	public Tasks UpdateTask(int TaskID, Tasks updatedTask){
-		
-		Tasks oldTask = null;
+	public Tasks UpdateTask(int taskID, Tasks updatedTask) throws Exception{
 		
 		try {
-			openWriterReader();
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
+			return myLogic.replaceTask(taskID, updatedTask);
+		} catch (IOException | InterruptedException e) {
+			throw e;
 		}
-		
-		try {
-			oldTask = SearchTaskID(TaskID);
-		} catch (IOException e2) {
-			e2.printStackTrace();
-		}
-		
-		try {
-			openWriterReader();
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		
-		try {
-			deleteTask(TaskID);
-			appendTask(updatedTask);
-		} 
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		try {
-			closeWriterReader();
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		
-		return oldTask;
 	}
 	
-	private int GenerateTaskID() {
-		
-		int largestID = 0;
-		ArrayList<Tasks> myTaskList = new ArrayList<Tasks>();
-		
+	//search and returns list of completed/incomplete tasks based on boolean input
+	public ArrayList<Tasks> SearchCompleteStatus(boolean complete) throws IOException{		
 		try {
-			openWriterReader();
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		
-		myTaskList = readFile();
-		for(int i=0; i<myTaskList.size(); i++) {
-			if(myTaskList.get(i).getTaskID() > largestID) {
-				largestID = myTaskList.get(i).getTaskID();
-			}
-		}
-		
-		try {
-			closeWriterReader();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return largestID +1;
+			return myLogic.searchComplete(complete);
+		} catch (IOException e) {
+			throw e;
+		}		
 	}
 	
-	private void closeWriterReader() throws IOException {
-		bw.close();
-		br.close();
+	//search and returns list of completed/incomplete tasks based on boolean input
+	public ArrayList<Tasks> SearchTaskType(int type) throws IOException{		
+		try {
+			return myLogic.searchTaskType(type);
+		} catch (IOException e) {
+			throw e;
+		}		
 	}
 	
-	public String getCurrentPath() {
-		return currentPath;
-	}
-
-	public void setCurrentPath(String currentPath) {
-		this.currentPath = currentPath;
-	}
-
-
 }
---
+++ StorageLogic.java
@@ -0,0 +1,565 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
+public class StorageLogic {
+		
+	private String currentPath = "";
+	
+	private File myFile;
+	private FileWriter fw;
+	private FileReader fr; 
+	private BufferedWriter bw;
+	private BufferedReader br;
+	
+	private final int DeadlineType = 1;
+	private final int DurationType = 2;
+	private final int BlockedType = 3;
+	private final int FloatingType = 4;
+	private final String defPath = "myTasks.txt";
+	
+	Gson gson = new Gson(); 
+	
+	//creates file at default directory, <local directory>/myTasks.txt
+	public void createDefaultFile() throws IOException {
+		
+		currentPath = defPath;
+		try {
+			createFile(currentPath);
+		} catch (IOException e) {
+			throw e;
+		}
+	}	
+	
+	//custom path and filename, path has to have escape characters
+	//eg. C:\\user\\Desktop\\myTasks.txt
+	public void createCustomFile(String path) throws IOException {
+		
+		currentPath = path;
+		
+		try {
+			createFile(currentPath);
+		} catch (IOException e) {
+			throw e;
+		}
+		
+	}
+	
+	//if file doesn't exist, creates file and returns null
+	//if file exists, read and return contents
+	private ArrayList<Tasks> createFile(String currentPath) throws IOException {
+		
+		ArrayList<Tasks> myTaskList = new ArrayList<Tasks>();
+		
+		//set up file
+		myFile = new File(currentPath);
+		
+		try {
+			CreateDirectories(myFile);
+		}
+		catch (IOException e) {
+			throw e;
+		}
+		
+		
+		//file exists, read file and return contents
+		if(myFile.exists() && myFile.isFile()) {
+			assert(myFile.exists());
+			myTaskList = getAllTasks();
+		}
+		//file not found, create file, intermediate directory? and return null
+		else {			
+			assert(myFile.exists() == false);
+			
+			try {
+				myFile.createNewFile();
+			} catch (IOException e) {
+				throw new IOException("File already exists or directory not made");
+			}
+		}
+		
+		return myTaskList;
+	}
+	
+	//checks if file contains path or just fileName.
+	//Creates whole path if it's a path. Returns true on success file+directory creation
+	private void CreateDirectories(File theFile) throws IOException {
+		
+		File parentFile = theFile.getParentFile();
+		
+		//String provided is a path, create directories
+		if(parentFile != null) {
+			//if creation of directories fail
+			if(!parentFile.mkdirs()) {
+				//because it doesn't exist
+				if(!parentFile.exists()) {
+					throw new IOException("Unable to create directories.");
+				}
+			}
+		}
+	}
+	
+	//reads and return all tasks in file
+	public ArrayList<Tasks> getAllTasks() throws IOException {
+		
+		try {
+			openWriterReader();
+		} catch (IOException e) {
+			throw e;
+		}
+		
+		
+		ArrayList<Tasks> myTaskList = new ArrayList<Tasks>();
+		String tempLine = "";
+		Tasks tempTask;
+		
+		//read contents of file		
+		try {
+			while ((tempLine = br.readLine()) != null) {
+				
+				//extract type				
+				int type = getTypeFromGsonString(tempLine);
+				
+				switch(type) {
+					case DeadlineType: 	tempTask = gson.fromJson(tempLine, DeadlineTask.class);
+										break;
+					case DurationType: 	tempTask = gson.fromJson(tempLine, DurationTask.class);
+										break;
+					case FloatingType: 	tempTask = gson.fromJson(tempLine, FloatingTask.class);
+										break;
+					case BlockedType: 	tempTask = gson.fromJson(tempLine, BlockedTask.class);
+										break;
+					default:			tempTask = null;
+										break;
+				}
+				myTaskList.add(tempTask);
+			}
+		}
+		catch (JsonSyntaxException e) {
+			throw e;
+		}
+		catch (IOException e) {
+			throw e;
+		}
+		
+		//close writer and reader
+		try {
+			closeWriterReader();
+		} 
+		catch (IOException e) {
+			throw e;
+		}
+		   
+		return myTaskList;
+	}
+
+	//retrieve type number from Gson string
+	private int getTypeFromGsonString(String gsonString) {
+		
+		int type = 0;
+		String splitter = "\"type\":";
+		
+		int startIndex = gsonString.indexOf(splitter)+splitter.length();
+		type = Integer.parseInt( gsonString.substring(startIndex, startIndex+1));
+		
+		return type;
+	}
+	
+	
+	//adds new task at the end of file, generates taskID if not present
+	public Tasks addNewTask(Tasks newTask) throws IOException {
+		
+		try {
+			openWriterReader();
+		} catch (IOException e) {
+			throw e;
+		}
+				
+		//if task has no taskID, generate one!
+		if(newTask.getTaskID() == 0) {
+			try {
+				newTask.setTaskID(GenerateTaskID());
+			} catch (IOException e) {
+				throw e;
+			}
+		}
+		
+		try {
+			closeWriterReader();
+		}
+		catch (IOException e) {
+			throw e;
+		}
+		
+		String tempLine = gson.toJson(newTask);
+		
+		try {
+			openWriterReader();
+		} catch (IOException e) {
+			throw e;
+		}
+		
+		//write new Tasks into file
+		try {
+			bw.write(tempLine);
+			bw.newLine();
+		}
+		catch (IOException e) {
+			throw e;
+		}
+		
+		//close writer
+		try {
+			closeWriterReader();
+		}
+		catch (IOException e) {
+			throw e;
+		}
+		
+		return newTask;
+	}
+	
+	//generate a valid taskID for requests
+	private int GenerateTaskID() throws IOException {
+		
+		int largestID = 0;
+		ArrayList<Tasks> myTaskList = new ArrayList<Tasks>();
+		
+		try {
+			openWriterReader();
+		} catch (IOException e) {
+			throw e;
+		}
+		
+		try {
+			myTaskList = getAllTasks();
+		} catch (IOException e) {
+			throw e;
+		}
+		for(int i=0; i<myTaskList.size(); i++) {
+			if(myTaskList.get(i).getTaskID() > largestID) {
+				largestID = myTaskList.get(i).getTaskID();
+			}
+		}
+		
+		try {
+			closeWriterReader();
+		}
+		catch (IOException e) {
+			throw e;
+		}
+		
+		return largestID +1;
+	}
+	
+	//open reader and writer
+	private void openWriterReader() throws IOException, FileNotFoundException{
+		//set up writers
+		try {
+			fw = new FileWriter(myFile.getAbsoluteFile(),true);
+			bw = new BufferedWriter(fw);
+		} catch (IOException e) {
+			throw new IOException("Unable to create writer for file");
+		}
+		try {
+			fr = new FileReader(myFile.getAbsoluteFile());
+			br = new BufferedReader(fr);
+		} catch (FileNotFoundException e) {
+			throw new FileNotFoundException("Reader cannot be created. File may be is missing");
+		}
+	}
+	
+	//close reader and writer
+	private void closeWriterReader() throws IOException {
+
+			try {
+				bw.close();
+			} catch (IOException e) {
+				throw new IOException("Unable to close writer");
+			}
+			try {
+				br.close();
+			} catch (IOException e) {
+				throw new IOException("Unable to close reader");
+			}
+	}
+
+	//searches and deletes the indicated taskID, if not found, returns null, if deleted returns deleted task 
+	public Tasks delete(int taskID) throws IOException, InterruptedException {
+		Tasks deletedTask = null;
+		ArrayList<Tasks> myTaskList = new ArrayList<Tasks>();
+		
+		try {
+			openWriterReader();
+		} catch (IOException e) {
+			throw e;
+		}
+		
+		boolean needDelete = false;
+		try {
+			myTaskList = getAllTasks();
+		} catch (IOException e) {
+			throw e;
+		}
+		
+		//find if taskID to be deleted is found
+		for(int i=0; i<myTaskList.size(); i++) {
+			if(myTaskList.get(i).getTaskID() == taskID) {
+				needDelete = true;
+				deletedTask = myTaskList.get(i);
+				myTaskList.remove(i);
+				break;
+			}
+		}
+		
+		//taskID to be deleted is found
+		if(needDelete) {
+			
+			try {
+				closeWriterReader();
+				System.gc();
+			}
+			catch (IOException e) {
+				throw e;
+			}
+			
+			//wait for garbage collector
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				throw e;
+			}
+			
+			//delete old file
+			if (!myFile.delete()) {
+		        System.out.println("Could not delete file");
+		    } 
+			
+			try {
+				createFile(currentPath);
+			} catch (IOException e) {
+				throw e;
+			}
+			
+			try {
+				openWriterReader();
+			} catch (IOException e) {
+				throw e;
+			}
+			
+			
+			
+			for(int i=0; i<myTaskList.size(); i++) {
+				String tempLine = gson.toJson(myTaskList.get(i)); 
+			
+				//write new Tasks into file
+				try {
+					bw.write(tempLine);
+					bw.newLine();
+				} 
+				catch (IOException e) {
+					throw e;
+				}
+			}
+		}
+		
+		try {
+			closeWriterReader();
+			System.gc();
+		}
+		catch (IOException e) {
+			throw e;
+		}
+			
+		return deletedTask;
+		
+	}
+
+	//searches task descriptiong for matching task and returns all matches, null if no match
+	public ArrayList<Tasks> searchDescription(String searchString) throws IOException {
+		
+		try {
+			openWriterReader();
+		} catch (IOException e) {
+			throw e;
+		}
+		
+		
+		ArrayList<Tasks> tempList;
+		try {
+			tempList = getAllTasks();
+		} catch (IOException e) {
+			throw e;
+		}
+		
+		ArrayList<Tasks> myTaskList = new ArrayList<Tasks>();
+		
+		for(int i=0; i<tempList.size(); i++) {
+			if(tempList.get(i).getDescription().toUpperCase().contains(searchString.toUpperCase())) {
+				myTaskList.add(tempList.get(i));
+			}
+		}
+		
+		try {
+			closeWriterReader();
+		}
+		catch (IOException e) {
+			throw e;
+		}
+		
+		return myTaskList;
+	}
+	
+	//search and returns task by taskID
+	public Tasks SearchTaskID(int taskID) throws FileNotFoundException, IOException{
+		
+		try {
+			openWriterReader();
+		}
+		catch (IOException e) {
+			throw e;
+		}
+
+		
+		ArrayList<Tasks> tempList = getAllTasks();
+		Tasks myTask = null;
+		
+		for(int i=0; i<tempList.size(); i++) {
+			if(tempList.get(i).getTaskID() == taskID) {
+				myTask = tempList.get(i);
+			}
+		}
+		
+		try {
+			closeWriterReader();
+		}
+		catch (IOException e) {
+			throw e;
+		}
+		
+		return myTask;
+	
+	}
+
+	//AKA update, replaces task with same taskID
+	public Tasks replaceTask(int taskID, Tasks updatedTask) throws IOException, InterruptedException {
+
+		Tasks oldTask = null;
+		
+		try {
+			openWriterReader();
+		} catch (IOException e) {
+			throw e;
+		}
+		
+		try {
+			oldTask = SearchTaskID(taskID);
+		} catch (IOException e) {
+			throw e;
+		}
+		
+		try {
+			openWriterReader();
+		} catch (IOException e) {
+			throw e;
+		}
+		
+		try {
+			delete(taskID);
+			addNewTask(updatedTask);
+		} 
+		catch (IOException e) {
+			throw e;
+		} catch (InterruptedException e) {
+			throw e;
+		}
+		
+		try {
+			closeWriterReader();
+		} catch (IOException e) {
+			throw e;
+		}
+		
+		return oldTask;
+	}
+	
+	//returns list of completed/incomplete tasks based on boolean input
+	public ArrayList<Tasks> searchComplete(boolean complete) throws IOException {
+		
+		ArrayList<Tasks> myTaskList = null;
+		
+		try {
+			openWriterReader();
+		}
+		catch (IOException e) {
+			throw e;
+		}
+
+		
+		ArrayList<Tasks> tempList = null;
+		try {
+			tempList = getAllTasks();
+		} catch (IOException e) {
+			throw e;
+		}
+		
+		for(int i=0; i<tempList.size(); i++) {
+			if(tempList.get(i).isCompleted() == complete) {
+				myTaskList.add(tempList.get(i));
+			}
+		}
+		
+		try {
+			closeWriterReader();
+		}
+		catch (IOException e) {
+			throw e;
+		}
+		
+		return myTaskList;
+	}
+
+	//returns list of specified tasks type
+	public ArrayList<Tasks> searchTaskType(int type) throws IOException {
+		ArrayList<Tasks> myTaskList = null;
+		
+		try {
+			openWriterReader();
+		}
+		catch (IOException e) {
+			throw e;
+		}
+
+		
+		ArrayList<Tasks> tempList = null;
+		try {
+			tempList = getAllTasks();
+		} catch (IOException e) {
+			throw e;
+		}
+		
+		for(int i=0; i<tempList.size(); i++) {
+			if(tempList.get(i).getType() == type) {
+				myTaskList.add(tempList.get(i));
+			}
+		}
+		
+		try {
+			closeWriterReader();
+		}
+		catch (IOException e) {
+			throw e;
+		}
+		
+		return myTaskList;
+	}	
+}
