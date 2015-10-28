ef3e26226e4dfde806fdbd3f25ae075f0898de62
LI ZAN <lizanmaggie@gmail.com>
2015-10-28 10:56:00 +0800

    fix UI typo

--- src/Time4WorkUI/Display.java
+++ src/Time4WorkUI/Display.java
@@ -7,73 +7,73 @@ import Time4WorkStorage.FloatingTask;
 import Time4WorkStorage.Tasks;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Display {
-
-	public Display(){
-
-	}
-
-	public String getStartDuration(Tasks task) {
-		Duration taskDuration = new Duration("", "", "", "");
-		String startDuration = "";
-		if (task instanceof DeadlineTask) {
-			// DeadlineTask classifiedTask = (DeadlineTask) task;
-			// taskDuration = classifiedTask.getDurationDetails();
-			return "";
-		} else if (task instanceof DurationTask) {
-			DurationTask classifiedTask = (DurationTask) task;
-			taskDuration = classifiedTask.getDurationDetails();
-		} else if (task instanceof FloatingTask) {
-			return "";
-		}
-		String date = taskDuration.getStartDate();
-		String time = taskDuration.getStartTime();
-		
-		if (time.length() < 3) {
-			time += "00";
-		}
-
-		startDuration += dateFormater(date + " " + time);
-		return startDuration;
-	}
-
-	public String getEndDuration(Tasks task) {
-		Duration taskDuration = new Duration("", "", "", "");
-		String endDuration = "";
-		if (task instanceof DeadlineTask) {
-			DeadlineTask classifiedTask = (DeadlineTask) task;
-			taskDuration = classifiedTask.getDurationDetails();
-		} else if (task instanceof DurationTask) {
-			DurationTask classifiedTask = (DurationTask) task;
-			taskDuration = classifiedTask.getDurationDetails();
-		} else if (task instanceof FloatingTask) {
-			return "";
-		}
-		String date = taskDuration.getEndDate();
-		String time = taskDuration.getEndTime();
-
-		if (time.length() < 3) {
-			time += "00";
-		}
-
-		endDuration += dateFormatter(date + " " + time);
-		return endDuration;
-	}
-
-	public String dateFormatter(String dateInString){
-		String dateFormatted = "";
-		SimpleDateFormat prevformatter = new SimpleDateFormat("ddMMyy HHmm");
-		SimpleDateFormat posformatter = new SimpleDateFormat("dd MMM yy, HH:mm");
-
-		try{
-			Date date = prevformatter.parse(dateInString);
-			dateFormatted = posformatter.format(date);
-			logger.log(Level.INFO, dateFormatted);
-		} catch(ParseException e){
-			e.printStackTrace();
-		}
-		return dateFormatted;
-	}
+    
+    public Display(){
+        
+    }
+    
+    public String getStartDuration(Tasks task) {
+        Duration taskDuration = new Duration("", "", "", "");
+        String startDuration = "";
+        if (task instanceof DeadlineTask) {
+            // DeadlineTask classifiedTask = (DeadlineTask) task;
+            // taskDuration = classifiedTask.getDurationDetails();
+            return "";
+        } else if (task instanceof DurationTask) {
+            DurationTask classifiedTask = (DurationTask) task;
+            taskDuration = classifiedTask.getDurationDetails();
+        } else if (task instanceof FloatingTask) {
+            return "";
+        }
+        String date = taskDuration.getStartDate();
+        String time = taskDuration.getStartTime();
+        
+        if (time.length() < 3) {
+            time += "00";
+        }
+        
+        startDuration += dateFormatter(date + " " + time);
+        return startDuration;
+    }
+    
+    public String getEndDuration(Tasks task) {
+        Duration taskDuration = new Duration("", "", "", "");
+        String endDuration = "";
+        if (task instanceof DeadlineTask) {
+            DeadlineTask classifiedTask = (DeadlineTask) task;
+            taskDuration = classifiedTask.getDurationDetails();
+        } else if (task instanceof DurationTask) {
+            DurationTask classifiedTask = (DurationTask) task;
+            taskDuration = classifiedTask.getDurationDetails();
+        } else if (task instanceof FloatingTask) {
+            return "";
+        }
+        String date = taskDuration.getEndDate();
+        String time = taskDuration.getEndTime();
+        
+        if (time.length() < 3) {
+            time += "00";
+        }
+        
+        endDuration += dateFormatter(date + " " + time);
+        return endDuration;
+    }
+    
+    public String dateFormatter(String dateInString){
+        String dateFormatted = "";
+        SimpleDateFormat prevformatter = new SimpleDateFormat("ddMMyy HHmm");
+        SimpleDateFormat posformatter = new SimpleDateFormat("dd MMM yy, HH:mm");
+        
+        try{
+            Date date = prevformatter.parse(dateInString);
+            dateFormatted = posformatter.format(date);
+        } catch(ParseException e){
+            e.printStackTrace();
+        }
+        return dateFormatted;
+    }
 }
 
