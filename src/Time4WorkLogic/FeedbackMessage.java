93ad55709c6411954b5e6c4325c33edd55cdfbc6
LI ZAN <lizanmaggie@gmail.com>
2015-10-29 18:03:14 +0800

    update FeedbackMessage to seperate complete and incomplete task

--- src/Time4WorkLogic/FeedbackMessage.java
+++ src/Time4WorkLogic/FeedbackMessage.java
@@ -3,21 +3,29 @@ import java.util.ArrayList;
 
 import Time4WorkStorage.Tasks;
 
+//@@author A0133894W
 public class FeedbackMessage {
     private String feedback;
-    private ArrayList<Tasks> taskList;
+    private ArrayList<Tasks> completeList;
+    private ArrayList<Tasks> incompleteList;
     
-    public FeedbackMessage(String feedback, ArrayList<Tasks> taskList) {
+    public FeedbackMessage(String feedback, ArrayList<Tasks> completeList,
+                           ArrayList<Tasks> incompleteList) {
         this.feedback = feedback;
-        this.taskList = taskList;
+        this.completeList = completeList;
+        this.incompleteList = incompleteList;
     }
     
     public String getFeedback() {
         return this.feedback;
     }
     
-    public ArrayList<Tasks> getTaskList() {
-        return this.taskList;
+    public ArrayList<Tasks> getCompleteTaskList() {
+        return this.completeList;
+    }
+    
+    public ArrayList<Tasks> getIncompleteTaskList() {
+        return this.incompleteList;
     }
     
 }
