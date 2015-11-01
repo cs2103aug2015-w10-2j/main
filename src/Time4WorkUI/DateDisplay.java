092178023c6846e4e5c03c941c78ec3757f8f321
DINH UYEN <dhpuyen@users.noreply.github.com>
2015-11-01 20:58:19 +0800

    Update and rename Display.java to DateDisplay.java

--- src/Time4WorkUI/DateDisplay.java
+++ src/Time4WorkUI/DateDisplay.java
@@ -1,21 +1,27 @@
 package Time4WorkUI;
 
+import Time4WorkLogic.FeedbackMessage;
+import Time4WorkLogic.Logic;
 import Time4WorkStorage.DeadlineTask;
 import Time4WorkStorage.Duration;
 import Time4WorkStorage.DurationTask;
 import Time4WorkStorage.FloatingTask;
 import Time4WorkStorage.Tasks;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class Display {
-    
-    public Display(){
-        
-    }
-    
-    public String getStartDuration(Tasks task) {
+public class DateDisplay {
+	private static final Logic logic = new Logic();
+	private static final Logger logger = Logger.getLogger(Logic.class.getName());
+
+	public DateDisplay(){
+	}
+
+	public String getStartDuration(Tasks task) {
         Duration taskDuration = new Duration("", "", "", "");
         String startDuration = "";
         if (task instanceof DeadlineTask) {
@@ -30,15 +36,11 @@ public class Display {
         }
         String date = taskDuration.getStartDate();
         String time = taskDuration.getStartTime();
-        
-        if (time.length() < 3) {
-            time += "00";
-        }
-        
-        startDuration += dateFormatter(date + " " + time);
+
+        startDuration += dateFormatter(date, time);
         return startDuration;
     }
-    
+
     public String getEndDuration(Tasks task) {
         Duration taskDuration = new Duration("", "", "", "");
         String endDuration = "";
@@ -53,27 +55,45 @@ public class Display {
         }
         String date = taskDuration.getEndDate();
         String time = taskDuration.getEndTime();
-        
-        if (time.length() < 3) {
-            time += "00";
-        }
-        
-        endDuration += dateFormatter(date + " " + time);
+
+        endDuration += dateFormatter(date, time);
         return endDuration;
     }
-    
-    public String dateFormatter(String dateInString){
-        String dateFormatted = "";
-        SimpleDateFormat prevformatter = new SimpleDateFormat("ddMMyy HHmm");
-        SimpleDateFormat posformatter = new SimpleDateFormat("dd MMM yy, HH:mm");
-        
-        try{
-            Date date = prevformatter.parse(dateInString);
-            dateFormatted = posformatter.format(date);
-        } catch(ParseException e){
-            e.printStackTrace();
-        }
-        return dateFormatted;
-    }
-}
 
+	public String dateFormatter(String dateInString, String timeInString){
+		String dateFormatted = "";
+		String timeFormatted = "";
+		SimpleDateFormat prevformatterDate = new SimpleDateFormat("ddMMyy");
+		SimpleDateFormat prevformatterTime = new SimpleDateFormat("HHmm");
+		SimpleDateFormat posformatterDate = new SimpleDateFormat("dd MMM yy,");
+		SimpleDateFormat posformatterTime = new SimpleDateFormat("HH:mm");
+		try{
+			if(!dateInString.equals("")){
+				Date date = prevformatterDate.parse(dateInString);
+				dateFormatted = posformatterDate.format(date);
+			} else {
+				dateFormatted = "";
+			}
+			if(!timeInString.equals("")){
+				Date time = prevformatterTime.parse(timeInString);
+				timeFormatted = posformatterTime.format(time);
+			} else {
+				timeFormatted = "";
+			}
+		} catch(ParseException e){
+			e.printStackTrace();
+		}
+		return dateFormatted + " " + timeFormatted;
+	}
+
+	public Date dateDurationFormatter(String dateDurationInString){
+		Date dateFormatted = null;
+		SimpleDateFormat prevformatter = new SimpleDateFormat("dd MMM yy, HH:mm");
+		try{
+			dateFormatted = prevformatter.parse(dateDurationInString);
+		} catch(ParseException e){
+			e.printStackTrace();
+		}
+		return dateFormatted;
+	}
+}
