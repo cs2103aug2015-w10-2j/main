b1b2761474217a39e6ddff1d12d553107e1ffb34
DINH UYEN <dhpuyen@users.noreply.github.com>
2015-11-04 02:12:54 +0800

    return displayed list, change tab to labels

--- src/Time4WorkUI/TaskController.java
+++ src/Time4WorkUI/TaskController.java
@@ -1,5 +1,6 @@
 package Time4WorkUI;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Stack;
 import java.util.logging.Level;
@@ -7,6 +8,7 @@ import java.util.logging.Logger;
 
 import Time4WorkLogic.FeedbackMessage;
 import Time4WorkLogic.Logic;
+import Time4WorkStorage.Storage;
 import Time4WorkStorage.Tasks;
 import Time4WorkUI.DateDisplay;
 import javafx.beans.value.ChangeListener;
@@ -25,26 +27,24 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.VBox;
+import Time4WorkUI.AutoCompleteComboBoxListener;
 
 public class TaskController {
 
 	private static Logic logic = new Logic();
-
 	// -----------------------------------------
 	// FXML variables
 	// -----------------------------------------
-
+	@FXML
+	private Label taskTitle;
 	@FXML
 	private VBox inputBox;
 	@FXML
 	private TextField userCommand;
 	@FXML
 	private Label feedback;
-
 	@FXML
-	private TableView<TaskModel> toDoTaskTable = new TableView<TaskModel>();
-	@FXML
-	private TableView<TaskModel> completedTaskTable = new TableView<TaskModel>();
+	private TableView<TaskModel> taskTable;
 
 	@FXML
 	private TableColumn<TaskModel, Integer> indexCol;
@@ -55,32 +55,12 @@ public class TaskController {
 	@FXML
 	private TableColumn<TaskModel, String> fromCol;
 
-	@FXML
-	private TableColumn<TaskModel, Integer> indexCol1;
-	@FXML
-	private TableColumn<TaskModel, String> descriptionCol1;
-	@FXML
-	private TableColumn<TaskModel, String> toCol1;
-	@FXML
-	private TableColumn<TaskModel, String> fromCol1;
-
-	@FXML
-	private TabPane tabPane = new TabPane();
-	@FXML
-	private Tab toDoTab;
-	@FXML
-	private Tab completedTab;
-
 
 	// -----------------------------------------------------
 	// Class variables
 	// -----------------------------------------------------
-	private ArrayList<Tasks> currentToDoList = new ArrayList<Tasks>();
-	private ArrayList<Tasks> currentCompletedList = new ArrayList<Tasks>();
-
-	private ArrayList<Tasks> currentList = new ArrayList<Tasks>();
-	SingleSelectionModel<Tab> selectionModel = tabPane.getSelectionModel();
-	private static final Logger logger = Logger.getLogger(Logic.class.getName());
+	private static ArrayList<Tasks> currentList = new ArrayList<Tasks>();
+	private static final Logger logger = Logger.getLogger(TaskController.class.getName());
 
 	/*
 	 * upStack and downStack are used to store user input to support using
@@ -92,9 +72,11 @@ public class TaskController {
 	// -------------------------------------------------------
 	// Message String
 	// -------------------------------------------------------
-
 	private static final String PROMPT_USERCOMMAND_TEXT = "Enter command";
 	private static final String PROMPT_USERCOMMAND_CLEAR = "";
+	private static final String TITLE_TODO_TASK = "TODO TASKS";
+	private static final String TITLE_COMPLETED_TASK = "COMPLETED TASKS";
+
 
 	/**
 	 * Initializes the controller class. This method is automatically called
@@ -110,74 +92,41 @@ public class TaskController {
 		indexCol.getStyleClass().add("align-center");
 		toCol.getStyleClass().add("align-center");
 		fromCol.getStyleClass().add("align-center");
-
-		indexCol1.getStyleClass().add("align-center");
-		toCol1.getStyleClass().add("align-center");
-		fromCol1.getStyleClass().add("align-center");
-
+		taskTitle.getStyleClass().add("taskTitle");
 
 		indexCol.setCellValueFactory(new PropertyValueFactory<>("index"));
 		descriptionCol.setCellValueFactory(new PropertyValueFactory<>("description"));
 		fromCol.setCellValueFactory(new PropertyValueFactory<>("startDuration"));
 		toCol.setCellValueFactory(new PropertyValueFactory<>("endDuration"));
-		toDoTaskTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+		taskTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
 
-		indexCol1.setCellValueFactory(new PropertyValueFactory<>("index"));
-		descriptionCol1.setCellValueFactory(new PropertyValueFactory<>("description"));
-		fromCol1.setCellValueFactory(new PropertyValueFactory<>("startDuration"));
-		toCol1.setCellValueFactory(new PropertyValueFactory<>("endDuration"));
-		completedTaskTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-
-
-		handleTabSelection();
 		initGUI();
 		handleUserInput();
 	}
 
+
 	public void handleUserInput() {
 		userCommand.setPromptText(PROMPT_USERCOMMAND_TEXT);
 		feedback.setText("Commands: add, delete, update, display, search, undo, store");
+
 		userCommand.setOnKeyPressed(e -> {
 			if (e.getCode().equals(KeyCode.ENTER)) {
 				String userInput = userCommand.getText();
-				
-			/**
-				tabPane.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Tab>() {
-					@Override
-					public void changed(ObservableValue<? extends Tab> arg0, Tab arg1, Tab arg2) {
-						if (userInput.toLowerCase().equals("search complete")) {
-							selectionModel.select(completedTab);
-						} else if (userInput.toLowerCase().equals("search incomplete")) {
-							selectionModel.select(toDoTab);
-						}
-					}
-				});
-			*/
 				upStack.push(userInput);
 				userCommand.setText(PROMPT_USERCOMMAND_CLEAR);
 				FeedbackMessage output;
 				try {
 					output = getOutputFromLogic(userInput);
-					currentToDoList = output.getIncompleteTaskList();
-					currentCompletedList = output.getCompleteTaskList();
-
-				//	 currentToDoList = logic.getIncompleteTaskFromMytaskList(logic.getFullTaskList());
-				//	 currentCompletedList = logic.getCompleteTaskFromMytaskList(logic.getFullTaskList());
-
-					toDoTaskTable.setItems(getTaskList(currentToDoList));
-					completedTaskTable.setItems(getTaskList(currentCompletedList));
-
-
-					toDoTab.setContent(toDoTaskTable);
-					completedTab.setContent(completedTaskTable);
-
-
+					currentList = output.getIncompleteTaskList();
+					if(userInput.toLowerCase().startsWith("display archive")){
+						currentList = output.getCompleteTaskList();
+						taskTitle.setText(TITLE_COMPLETED_TASK);
+					} else if (userInput.toLowerCase().startsWith("display incomplete")) {
+						currentList = output.getIncompleteTaskList();
+						taskTitle.setText(TITLE_TODO_TASK);
+					}
+					taskTable.setItems(getTaskList(currentList));
 					feedback.setText(output.getFeedback());
-					handleTabSelection();
-					logger.log(Level.INFO, "toDoTab selected::" + toDoTab.isSelected());
-					logger.log(Level.INFO, "completedTab selected:: " + completedTab.isSelected());
-					logger.log(Level.INFO, "toDoTab selected .." + currentList.size());
-					logger.log(Level.INFO, "completedTab selected:: " + currentList.size());
 				} catch (Exception e1) {
 					e1.printStackTrace();
 				}
@@ -194,12 +143,10 @@ public class TaskController {
 			}
 
 		});
-
 	}
 
 	public ObservableList<TaskModel> getTaskList(ArrayList<Tasks> currentList) {
 		ObservableList<TaskModel> taskData = FXCollections.observableArrayList();
-
 		DateDisplay display = new DateDisplay();
 		for (int i = 0; i < currentList.size(); i++) {
 			Tasks currentTask = currentList.get(i);
@@ -209,72 +156,20 @@ public class TaskController {
 		return taskData;
 	}
 
-	public void initGUI() {
-		currentToDoList = logic.getIncompleteTaskList();
-		currentCompletedList = logic.getCompleteTaskList();
-
-
-		toDoTaskTable.setItems(getTaskList(currentToDoList));
-		completedTaskTable.setItems(getTaskList(currentCompletedList));
-
-		toDoTab.setContent(toDoTaskTable);
-		completedTab.setContent(completedTaskTable);
-
+	public void initGUI() throws IOException {
+		taskTitle.setText(TITLE_TODO_TASK);
+		currentList = logic.getIncompleteTaskList();
+		taskTable.setItems(getTaskList(currentList));
+		logger.log(Level.INFO, "currentLIst size(init) is " + this.currentList.size());
 	}
 
 	public FeedbackMessage getOutputFromLogic(String userCommand) throws Exception {
 		return logic.executeCommand(userCommand);
 	}
 
-	public int getTaskIDFromUserInput(int userInputID) {
-		Tasks requiredTask = currentList.get(userInputID - 1);
-		return requiredTask.getTaskID();
+	public static ArrayList<Tasks> getDisplayedList() {
+		logger.log(Level.INFO, "currentLIst size is " + currentList.size());
+		return currentList;
 	}
 
-	@FXML
-	public void toDoTabSelected(Event e) {
-		currentList = currentToDoList;
-	}
-
-	@FXML
-	public void completedTabSelected(Event e) {
-		currentList = currentCompletedList;
-	}
-
-	public void handleTabSelection() {
-		tabPane.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Tab>() {
-			@Override
-			public void changed(ObservableValue<? extends Tab> arg0, Tab arg1, Tab mostRecentlySelectedTab) {
-				if (mostRecentlySelectedTab.equals(toDoTab)) {
-					currentList = currentToDoList;
-				}
-				if (mostRecentlySelectedTab.equals(completedTab)) {
-					currentList = currentCompletedList;
-				}
-			}
-		});
-	}
-
-	public ArrayList<Tasks> getFullList(ArrayList<Tasks> incompletedList, ArrayList<Tasks> completedList){
-		ArrayList<Tasks> list = new ArrayList<Tasks>();
-		for(int i=0; i<incompletedList.size(); i++){
-			Tasks task = incompletedList.get(i);
-			list.add(task);
-		}
-
-		for(int i=0; i<completedList.size(); i++){
-			Tasks task = completedList.get(i);
-			list.add(task);
-		}
-		return list;
-	}
-/*
-	public void handleMousePress(Event e) {
-		tabPane.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent mouseEvent) {
-			
-			}
-		});
-	}*/
 }
