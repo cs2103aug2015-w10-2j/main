1e402ca19eb75363f18fd01d766fdeca88dfdcda
DINH UYEN <dhpuyen@users.noreply.github.com>
2015-11-08 01:03:08 +0800

    help command and refractoring

--- src/Time4WorkUI/TaskController.java
+++ src/Time4WorkUI/TaskController.java
@@ -1,16 +1,16 @@
 package Time4WorkUI;
 
 import java.io.IOException;
+
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Stack;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import Time4WorkLogic.FeedbackMessage;
-import Time4WorkLogic.Logic;
+import Time4WorkLogic.*;
 import Time4WorkStorage.Tasks;
-import Time4WorkUI.DateDisplay;
+import Time4WorkUI.*;
 import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -26,14 +26,14 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.VBox;
 import javafx.util.Callback;
-import Time4WorkUI.UserInput;
-import Time4WorkUI.FullValueCell;
+
 
 //@@author: A0112077N
 public class TaskController {
 
 	private static Logic logic = new Logic();
 	private static UserInput userInput;
+	private static PopUpMenu help = new PopUpMenu();
 
 	// -----------------------------------------
 	// FXML variables
@@ -61,11 +61,10 @@ public class TaskController {
 	// Class variables
 	// -----------------------------------------------------
 	private static ArrayList<Tasks> currentList = new ArrayList<Tasks>();
-	private static final Logger logger = Logger.getLogger(TaskController.class.getName());
-
+	 private static final Logger logger = Logger.getLogger(TaskController.class.getName());
 	/*
 	 * upStack and downStack are used to store user input to support using
-	 * 'UP'/'DOWN' keycode to obtain previous/post user command
+	 * 'UP'/'DOWN' arrow to obtain previous/post user command
 	 */
 	private Stack<String> upStack = new Stack<String>();
 	private Stack<String> downStack = new Stack<String>();
@@ -76,7 +75,7 @@ public class TaskController {
 	private static final String PROMPT_USERCOMMAND_TEXT = "Enter command";
 	private static final String PROMPT_USERCOMMAND_CLEAR = "";
 	private static final String NO_CONTENT_TABLE_MESSAGE = "You have no tasks.";
-
+	private static final String INIT_UI = "init ui";
 	/**
 	 * Initializes the controller class. This method is automatically called
 	 * after the fxml file has been loaded.
@@ -87,6 +86,7 @@ public class TaskController {
 	 */
 	@FXML
 	private void initialize() throws Exception {
+		logger.log(Level.INFO, INIT_UI);
 		initColumn();
 		initTableView();
 		setUpUI();
@@ -98,8 +98,10 @@ public class TaskController {
 		feedback.setText("Commands: add, delete, update, display, search, undo, store");
 
 		userCommand.setOnKeyPressed(e -> {
+			userInput = new UserInput(userCommand.getText());
+			autoCompleteUserCommand(userInput);
+
 			if (e.getCode().equals(KeyCode.ENTER)) {
-				userInput = new UserInput(userCommand.getText());
 				upStack.push(userInput.getUserInput());
 				userCommand.setText(PROMPT_USERCOMMAND_CLEAR);
 
@@ -108,11 +110,13 @@ public class TaskController {
 					output = getOutputFromLogic(userInput.getUserInput());
 					currentList = logic.getIncompleteTaskList();
 
-					//show the list of completed tasks for "display archive" cmd
+					// show the list of completed tasks for "display archive" command
 					if (userInput.isDisplayArchiveCommand()) {
 						currentList = output.getCompleteTaskList();
-					} else {
+					} else if (userInput.isDisplayInCompleteCommand()){
 						currentList = output.getIncompleteTaskList();
+					} else if(userInput.isHelpCommand()){
+						help.showMenu();
 					}
 
 					taskTable.setItems(getObservableTaskList(currentList));
@@ -123,6 +127,7 @@ public class TaskController {
 				}
 			}
 
+			// press UP/DOWN arrow to get previous/post user input
 			if (e.getCode().equals(KeyCode.UP)) {
 				String currentCommand = upStack.pop();
 				userCommand.setText(currentCommand);
@@ -132,12 +137,22 @@ public class TaskController {
 				userCommand.setText(currentCommand);
 				upStack.push(currentCommand);
 			}
+
+			// Shortcut to open help sheet
+			if (e.getCode().equals(KeyCode.F1)){
+				help.showMenu();
+			}
+
+			//Shortcut to close the program
+			if (e.getCode().equals(KeyCode.ESCAPE)){
+				System.exit(0);
+			}
 		});
 	}
 
-
 	/**
 	 * Get the observableList to display in UI
+	 *
 	 * @param currentList
 	 * @return taskData
 	 */
@@ -148,6 +163,7 @@ public class TaskController {
 			Tasks currentTask = currentList.get(i);
 			taskData.add(new TaskModel(i + 1, currentTask.getDescription(), display.getStartDuration(currentTask),
 					display.getEndDuration(currentTask)));
+
 		}
 		return taskData;
 	}
@@ -169,20 +185,28 @@ public class TaskController {
 		fromCol.setCellValueFactory(new PropertyValueFactory<>("startDuration"));
 		toCol.setCellValueFactory(new PropertyValueFactory<>("endDuration"));
 
-		descriptionCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<TaskModel,String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(CellDataFeatures<TaskModel, String> param) {
-                return new ReadOnlyStringWrapper(param.getValue().getDescription());
-            }
-        });
+		createTooltipForDescriptionCol();
+	}
 
+	/**
+	 * Create tooltip to show the whole description of the task
+	 * This is useful when description content is too long
+	 */
+	public void createTooltipForDescriptionCol(){
+		descriptionCol.setCellValueFactory(
+				new Callback<TableColumn.CellDataFeatures<TaskModel, String>, ObservableValue<String>>() {
+					@Override
+					public ObservableValue<String> call(CellDataFeatures<TaskModel, String> param) {
+						return new ReadOnlyStringWrapper(param.getValue().getDescription());
+					}
+				});
 
-		 descriptionCol.setCellFactory(new Callback<TableColumn<TaskModel,String>, TableCell<TaskModel,String>>() {
-	            @Override
-	            public TableCell<TaskModel, String> call(TableColumn<TaskModel, String> param) {
-	                return new FullValueCell();
-	            }
-	        });
+		descriptionCol.setCellFactory(new Callback<TableColumn<TaskModel, String>, TableCell<TaskModel, String>>() {
+			@Override
+			public TableCell<TaskModel, String> call(TableColumn<TaskModel, String> param) {
+				return new FullValueCell();
+			}
+		});
 	}
 
 	public void initTableView() {
@@ -190,12 +214,8 @@ public class TaskController {
 		taskTable.setPlaceholder(noContentLabel);
 	}
 
-	public void setUpUI() throws IOException {
-		try {
-			currentList = logic.getIncompleteTaskList();
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
+	public void setUpUI() throws IOException, ParseException {
+		currentList = logic.getIncompleteTaskList();
 		taskTable.setItems(getObservableTaskList(currentList));
 	}
 
@@ -203,7 +223,18 @@ public class TaskController {
 		return logic.executeCommand(userCommand);
 	}
 
+
+	/**
+	 * To support commands that require list no. of the the displayed list
+	 * @return the displayed task list
+	 */
 	public static ArrayList<Tasks> getDisplayedList() {
 		return currentList;
+	}
+
+	public void autoCompleteUserCommand(UserInput input){
+		if (input.isAddCommand()){
+			userCommand.setText("add");
+		}
 	}
 }
