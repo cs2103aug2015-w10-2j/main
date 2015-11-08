a452f6a2b3476f16c652e9762eecfe2dedf8ba75
LI ZAN <lizanmaggie@gmail.com>
2015-11-08 15:47:20 +0800

    fix display bug

--- src/Time4WorkUI/TaskController.java
+++ src/Time4WorkUI/TaskController.java
@@ -119,6 +119,7 @@ public class TaskController {
 						currentList = output.getIncompleteTaskList();
 					}
 
+					currentList = output.getIncompleteTaskList();
 					taskTable.setItems(getObservableTaskList(currentList));
 					feedback.setText(output.getFeedback());
 
